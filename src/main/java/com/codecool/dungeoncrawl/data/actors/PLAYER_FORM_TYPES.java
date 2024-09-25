9da194fc29a4b439857bd8a4320e19ad6ac609a3
o-k-e <123556323+o-k-e@users.noreply.github.com>
2024-09-25 07:13:18 +0200

    add getEnumByTileName() to PLAYER_FORM_TYPES class needed for loading the game status

--- src/main/java/com/codecool/dungeoncrawl/data/actors/PLAYER_FORM_TYPES.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/PLAYER_FORM_TYPES.java
@@ -3,18 +3,28 @@ package com.codecool.dungeoncrawl.data.actors;
 import com.codecool.dungeoncrawl.ui.Tiles;
 
 public enum PLAYER_FORM_TYPES {
-PLAYER_SHIELD_SWORD("playerShieldSword"),
-    PLAYER_BASIC ("playerBasic"),
+    PLAYER_SHIELD_SWORD("playerShieldSword"),
+    PLAYER_BASIC("playerBasic"),
     PLAYER_SWORD("playerSword"),
     PLAYER_SWORD_SHIELD_HELMET("playerSwordShieldHelmet"),
     PLAYER_SUPERSWORD_SHIELD_HELMET("playerSuperSwordShieldHelmet"),
-    PLAYER_HELMET ("playerHelmet"),
-    PLAYER_SUPER("playerSuper"),;
+    PLAYER_HELMET("playerHelmet"),
+    PLAYER_SUPER("playerSuper"),
+    ;
 
-private String tileName;
+    private String tileName;
 
     PLAYER_FORM_TYPES(String tileName) {
         this.tileName = tileName;
+    }
+
+    public static PLAYER_FORM_TYPES getEnumByTileName(String tileName) {
+        for (PLAYER_FORM_TYPES value : PLAYER_FORM_TYPES.values()) {
+            if (value.getTileName().equals(tileName)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid tile name: " + tileName);
     }
 
     public String getTileName() {
