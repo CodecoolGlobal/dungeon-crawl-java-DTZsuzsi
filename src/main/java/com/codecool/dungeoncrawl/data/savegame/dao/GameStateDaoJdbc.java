d971afcd35635ab0b5aa15c6614ff6d6a061f1ff
o-k-e <123556323+o-k-e@users.noreply.github.com>
2024-09-25 04:59:01 +0200

    add DatabaseManager to establish db connection

--- src/main/java/com/codecool/dungeoncrawl/data/savegame/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/data/savegame/dao/GameStateDaoJdbc.java
@@ -1,4 +1,23 @@
 package com.codecool.dungeoncrawl.data.savegame.dao;
 
-public class GameStateDaoJdbc {
+import com.codecool.dungeoncrawl.data.savegame.GameState;
+
+import javax.sql.DataSource;
+
+public class GameStateDaoJdbc implements GameStateDao {
+
+    private DataSource dataSource;
+    public GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource; //dataSource is used to connect to the database. This is passed into the class via the constructor and will be used in each method to get a connection to the database.
+    }                                   //This sets up the DAO to be able to interact with the database.
+
+    @Override
+    public void save(GameState gameState) {
+
+    }
+
+    @Override
+    public GameState load() {
+        return null;
+    }
 }
