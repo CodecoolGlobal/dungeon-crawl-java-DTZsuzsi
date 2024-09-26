560d4b93cbcb2dd5da60da605e545b8af3a8b3e6
o-k-e <123556323+o-k-e@users.noreply.github.com>
2024-09-27 00:28:20 +0200

    add monster roar when boss dies

--- src/main/java/com/codecool/dungeoncrawl/data/actors/npc/monsters/Boss.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/npc/monsters/Boss.java
@@ -17,31 +17,62 @@ public class Boss extends Monsters {
     }
 
     @Override
-    public  void interact(Player player){
+    public void interact(Player player) {
 
         while (health >= 0 && player.getHealth() >= 0) {
-            this.setHealth(health-player.getAttack());
-            player.setHealth(player.getHealth()-attack);
-
+            this.setHealth(health - player.getAttack());
+            player.setHealth(player.getHealth() - attack);
         }
+
         if (player.getHealth() <= 0) {
             this.action.showPopup("Game over", "Sorry, you've died! Game over!");
+            SoundPlayer gameOver = new SoundPlayer(SOUND_TYPES.GAME_OVER);
+            gameOver.play();
+            Timeline timeline = new Timeline(new KeyFrame(
+                    Duration.seconds(14),
+                    event -> System.exit(0)
+            ));
+            timeline.setCycleCount(1);
+            timeline.play();
         }
 
         if (this.getHealth() <= 0) {
             this.getCell().setActor(null);
             this.getCell().setType(CellType.BOSSFLOOR);
+
+            // Play monster roar first
+            SoundPlayer monsterRoar = new SoundPlayer(SOUND_TYPES.MONSTER_ROAR);
+            monsterRoar.play();
             action.showPopup("Winning", "Congratulations! You won!");
-            SoundPlayer winnerSound = new SoundPlayer(SOUND_TYPES.WINNER);
-            winnerSound.play();
-            Timeline timeline = new Timeline(new KeyFrame(
-                    Duration.seconds(14),
-                    event -> System.exit(0)
-            ));
 
-            timeline.setCycleCount(1);
-            timeline.play();
+            // Chain the events: first, wait for the roar to finish, then play the winner sound
+            Timeline soundTimeline = new Timeline(
+                    new KeyFrame(Duration.seconds(2), event -> {
+                        // Play winner sound after roar finishes
+                        SoundPlayer winnerSound = new SoundPlayer(SOUND_TYPES.WINNER);
+                        winnerSound.play();
 
+                        // Schedule winner sound to stop after 14 seconds
+                        Timeline stopWinnerSoundTimeline = new Timeline(new KeyFrame(
+                                Duration.seconds(14),
+                                e -> {
+                                    winnerSound.stop();
+                                }
+                        ));
+                        stopWinnerSoundTimeline.setCycleCount(1);
+                        stopWinnerSoundTimeline.play();
+
+                        // Exit the game after showing the popup and allowing the winner sound to finish
+                        Timeline exitTimeline = new Timeline(new KeyFrame(
+                                Duration.seconds(16), // Wait slightly longer than the winner sound to ensure popup is visible
+                                e -> System.exit(0)
+                        ));
+                        exitTimeline.setCycleCount(1);
+                        exitTimeline.play();
+                    })
+            );
+            soundTimeline.setCycleCount(1);
+            soundTimeline.play();
         }
     }
 
--- src/main/java/com/codecool/dungeoncrawl/logic/SOUND_TYPES.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/SOUND_TYPES.java
@@ -3,7 +3,8 @@ package com.codecool.dungeoncrawl.logic;
 public enum SOUND_TYPES {
     WINNER("/sound/free-music-in-my-mind-remake-26367.mp3"),
     START("/sound/robotic-countdown-43935.mp3"),
-    GAME_OVER("/stinger-3-38177.mp3"),
+    GAME_OVER("/sound/stinger-3-38177.mp3"),
+    MONSTER_ROAR("/sound/low-monster-roar-97413.mp3"),
     NEW_MAP("/sound/get_ready_to_the_next_fight_sielxm3d-83870.mp3");
 
 
